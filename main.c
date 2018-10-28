1ccac983a3beaecdf7e61c17eaf41ccff7ce9c4c
Adrian Brodzik <adrianbrodzyk@gmail.com>
2018-10-28 16:37:41 +0100

    Code cleanup.

--- main.c
+++ main.c
@@ -90,8 +90,10 @@ bool isMagicSquare(int n, int *tab)
 }
 
 // Generates all possible permutations of a square matrix
-void bruteForcePermutations(int n, int *tab, int index)
+// Source: https://www.geeksforgeeks.org/write-a-c-program-to-print-all-permutations-of-a-given-string/
+void permutate(int n, int *tab, int index)
 {
+    // Finish recursion
     if (index == n*n)
     {
         if (isMagicSquare(n, tab))
@@ -99,41 +101,50 @@ void bruteForcePermutations(int n, int *tab, int index)
             printSquare(n, tab);
         }
     }
+    // Continue recursion
     else
     {
+        // Permutate non-fixed characters
         for (int i = index; i < n*n; ++i)
         {
             swap(tab + i, tab + index);
-            bruteForcePermutations(n, tab, index + 1);
+            permutate(n, tab, index + 1);
             swap(tab + i, tab + index);
         }
     }
 }
 
-// Generates all permutations of 'tab' and calls 'callback' after each permutation.
-void smartPermutations(int size, int *tab, int *data, int index, void (*callback)(int, int*, int*, int*), int *counter)
+// Generates all permutations of 'tab' and calls 'callback' after each permutation,
+// 'data' variable is used to pass extra information to the 'callback' function,
+// 'callback' arguments: size, tab, data, counter
+void advancedPermutate(int size, int *tab, int *data, int index, void (*callback)(int, int*, int*, int*), int *counter)
 {
+    // Limit magic square output
     if (*counter == 5)
     {
         return;
     }
 
+    // Finish recursion
     if (index == size)
     {
         callback(size, tab, data, counter);
     }
+    // Continue recursion
     else
     {
+        // Permutate non-fixed characters
         for (int i = index; i < size; ++i)
         {
             swap(tab + i, tab + index);
-            smartPermutations(size, tab, data, index + 1, callback, counter);
+            advancedPermutate(size, tab, data, index + 1, callback, counter);
             swap(tab + i, tab + index);
         }
     }
 }
 
-// Based on 'create5x5'
+// Creates a 4x4 magic square
+// Source: http://www.magic-squares.net/pandiag5.htm
 void create4x4(int size, int *x, int *X, int *counter)
 {
     int magic[] = {
@@ -148,6 +159,7 @@ void create4x4(int size, int *x, int *X, int *counter)
     ++(*counter);
 }
 
+// Creates a 5x5 magic square
 // Source: http://www.magic-squares.net/pandiag5.htm
 void create5x5(int size, int *x, int *X, int *counter)
 {
@@ -164,15 +176,16 @@ void create5x5(int size, int *x, int *X, int *counter)
     ++(*counter);
 }
 
-void innerPermutations(int size, int *tab, int *data, int *counter)
+// Used as a 'callback' function for 'advancedPermutate' to permutate 'data'
+void innerPermutation(int size, int *tab, int *data, int *counter)
 {
     if (size == 4)
     {
-        smartPermutations(size, data, tab, 0, create4x4, counter);
+        advancedPermutate(size, data, tab, 0, create4x4, counter);
     }
     else if (size == 5)
     {
-        smartPermutations(size, data, tab, 0, create5x5, counter);
+        advancedPermutate(size, data, tab, 0, create5x5, counter);
     }
     else
     {
@@ -191,7 +204,7 @@ int main()
         7, 8, 9
     };
 
-    bruteForcePermutations(3, tab3x3, 0);
+    permutate(3, tab3x3, 0);
 
     printf("---------- n = 4 ----------\n\n");
 
@@ -199,7 +212,7 @@ int main()
     int tab4x4_2[] = { 1, 2, 3, 4 };
     int counter4x4 = 0;
 
-    smartPermutations(4, tab4x4_1, tab4x4_2, 0, innerPermutations, &counter4x4);
+    advancedPermutate(4, tab4x4_1, tab4x4_2, 0, innerPermutation, &counter4x4);
 
     printf("---------- n = 5 ----------\n\n");
 
@@ -207,7 +220,7 @@ int main()
     int tab5x5_2[] = { 1, 2, 3, 4, 5 };
     int counter5x5 = 0;
 
-    smartPermutations(5, tab5x5_1, tab5x5_2, 0, innerPermutations, &counter5x5);
+    advancedPermutate(5, tab5x5_1, tab5x5_2, 0, innerPermutation, &counter5x5);
 
     return 0;
 }
